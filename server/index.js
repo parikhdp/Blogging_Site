3544118ce7f0543d1afe86f010285144f2ceaf36
Miit Daga <129889853+miit-daga@users.noreply.github.com>
2024-10-05 21:21:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const https = require('https');
-const fs = require('fs');
 const app = express();
 const cors = require('cors');
 const { default: mongoose } = require('mongoose');
